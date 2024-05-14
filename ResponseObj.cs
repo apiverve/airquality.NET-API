263e69c0a2ed0b65d55c82a6b66121be0380250f
APIVerve Team <hello@apiverve.com>
2024-05-14 15:20:25 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,53 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class data
+{
+    [JsonProperty("pm2_5")]
+    public double pm25 { get; set; }
+
+    [JsonProperty("pm10")]
+    public double pm10 { get; set; }
+
+    [JsonProperty("us-epa-index")]
+    public int usepaindex { get; set; }
+
+    [JsonProperty("gb-defra-index")]
+    public int gbdefraindex { get; set; }
+
+    [JsonProperty("carbonMonoxide")]
+    public double carbonMonoxide { get; set; }
+
+    [JsonProperty("ozone")]
+    public double ozone { get; set; }
+
+    [JsonProperty("nitrogenDioxide")]
+    public double nitrogenDioxide { get; set; }
+
+    [JsonProperty("sulfurdioxide")]
+    public double sulfurdioxide { get; set; }
+
+    [JsonProperty("recommendation")]
+    public string recommendation { get; set; }
+
+    [JsonProperty("city")]
+    public string city { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
